5dd50638a22ef758863609dab8c0c9a5cf035479
IvyJebet <jebetivy388@gmail.com>
2023-09-03 21:06:44 -0700

    Strings Innit!

--- C_Programming/strings.c
+++ C_Programming/strings.c
@@ -6,12 +6,15 @@ int main()
 	char greeting[] = "Asalam Aleikum!, ";
 	char name[] = "Jebet Ivy";
 	char combined[30];
+	char thirdChar;
+	thirdChar = name[2];
 
 	strcpy(combined, greeting);
 	strcat(combined, name);
 
 	printf("The Combined string is: %s\n", combined);
 	printf("The length of the combined string is: %ld\n", strlen(combined));
+	printf("The third character of name: %c\n", thirdChar);
 
 	return (0);
 }
