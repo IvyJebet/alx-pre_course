1d1d82fd7cfb5f227f1c712b08414d1c36ad2eaf
IvyJebet <jebetivy388@gmail.com>
2023-09-08 20:33:31 -0700

    If-Else Innit!

---
+++ C_Programming/ifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 15;
+
+	if (age >= 18)
+	{
+		printf("You are eligible to vote!\n");
+	}
+	else
+	{
+		printf("Rudi home, We Mzee!\n");
+	}
+	printf("Have a splendid day!\n");
+	return (0);
+}
